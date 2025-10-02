a0d40ac1028a906a47cd492cb556595f1897bd60
scott <rowekast@mail.uc.edu>
2025-10-02 15:20:39 -0400

    complete lab 6 part 4

---
+++ src/HighorLow.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HighorLow {
+    static void main() {
+
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random(1-10);
+        int targetNum;
+        int guessNum;
+
+        targetNum = rand.nextInt();
+
+        boolean done = false;
+        String trash = "";
+
+        do {
+            System.out.print("Enter your guess (1-10): ");
+            if (in.hasNextInt()) {
+                guessNum = in.nextInt();
+                in.nextLine();
+                if (guessNum < 1 || guessNum > 10) {
+                    System.out.println("Guess must be between 1 and 10, please enter a valid number.");
+                } else if (guessNum < targetNum) {
+                    System.out.println("Too low, try again.");
+                } else if (guessNum > targetNum) {
+                    System.out.println("Too high, try again.");
+                } else {
+                    System.out.println("Congratulations! You guessed the correct number: " + targetNum);
+                    done = true;
+                }
+            } else {
+                trash = in.nextLine();
+                System.out.println(trash + " is not a valid number, please enter a valid number.");
+            }
+
+        }while (!done)
+    }
+}
